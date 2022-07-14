9bd13e155dfcfdc387fdfae1e7905a2932deb35b
lehuyloc007 <79748205+lehuyloc007@users.noreply.github.com>
2022-07-14 16:34:08 +0700

    add slices

--- 6_array_slices.go
+++ 6_array_slices.go
@@ -18,7 +18,55 @@ func main6() {
 	b := [3]int{2, 3, 4}
 	fmt.Println(b)
 
-	//Slices là con trỏ tới mảng
 	//Để giải quyết vấn đề nguyên tắc của mảng là length cố định thì ta có slices
+	//Slices tương tự như mảng, nhưng mạnh mẽ và linh hoạt hơn.
+	//Giống như mảng, slices cũng được sử dụng để lưu trữ nhiều giá trị cùng kiểu trong một biến duy nhất.
+	//Tuy nhiên chiều dài của slices có thể lớn hơn và nhỏ hơn
 
+	//Kb1: Khai báo 1 slices rỗng
+	var myslice []int // ?? khai báo myslice := []int lỗi
+	//slices là con trỏ vậy nên giá trị mặc định của nó là nil
+	var h []int
+	if h == nil {
+		fmt.Println("nil!")
+	}
+	//i là con trỏ nên cũng có kiểu nil
+	var i *int
+	if i == nil {
+		fmt.Println("nil!")
+	}
+
+	fmt.Println(myslice)
+	//Kb2: Khai báo 1 sliecs và gán giá trị cho nó
+	c := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	fmt.Println(c)
+
+	//Slices là con trỏ trỏ tới tới mảng
+	// khai báo và lấy đến phần tử thứ 4 kể từ phần tử đầu tiên
+	d := c[:4] //cách 2 var d []int = primes[:4]
+	fmt.Println(d)
+
+	// khai báo và lấy ra phần tử ở vị trí thứ 1 đến phần tử thứ 4 thì dừng lại
+	e := c[1:4] //cách 2 var e []int = primes[1:4]
+	fmt.Println(e)
+
+	// khai báo và lấy ra phần tử từ vị trí thứ 3
+	f := c[3:] //cách 2 e []int = primes[3:]
+	fmt.Println(f)
+
+	//Slice bản chất là cấu trúc dữ liêu
+	//Bên trong nó là 1 cái mảng , length(len) và capacity(cap)
+	// Trong đó: len() là độ chứa hiện tại, cap() là độ chứa tối đa
+	g := []int{2, 3, 4, 5, 9, 7, 8}
+	fmt.Printf("len=%d cap=%d %v\n", len(g), cap(g), g)
+	g = g[:4]
+	fmt.Printf("len=%d cap=%d %v\n", len(g), cap(g), g)
+
+	//Kb3: Khai báo slice cùng với make name := make([]datatype, len, cap)
+	//cách1: khai báo slice có len và cap bằng nhau
+	k := make([]int, 5)
+	fmt.Printf("len=%d cap=%d %v\n", len(k), cap(k), k)
+	//cách1: khai báo slice có len và cap khác nhau
+	l := make([]int, 2, 5)
+	fmt.Printf("len=%d cap=%d %v\n", len(l), cap(l), l)
 }
