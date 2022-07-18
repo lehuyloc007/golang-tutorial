c1353a9a891345b98de9d14d4d49d8e20486a4c7
lehuyloc007 <79748205+lehuyloc007@users.noreply.github.com>
2022-07-18 23:28:56 +0700

    update arrslice

--- 7_functionvalue.go
+++ 7_functionvalue.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 )
 
+// function cũng coi là 1 giá trị truyền vào hàm
+// còn gọi là con trỏ hàm
+func compute(fn func(float64, float64) float64) float64 {
+	return fn(3, 4)
+}
 func main7() {
 
 	fmt.Println("hello")
